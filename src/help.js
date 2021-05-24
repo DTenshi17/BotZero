73a6d6238a3dee32322abce4344604526716137b
KidoBot <luisx469145@gmail.com>
2021-05-24 08:23:04 +0800

    First commit

---
+++ src/help.js
@@ -0,0 +1,84 @@
+const help = (prefix) => {
+	return `
+「 *Zero two* 」
+
+◪ *INFORMACION*
+  ❏ Comando: 「  ${prefix}  」
+  ❏ Admi: Capi Kido
+  ❏ Creador: Shan
+  ❏ Bot: Zero
+
+◪ *PARA USAR EL BOT*
+  ❏ Registrate con el comando ${prefix}daftar y tu nombre
+
+◪ *NUEVA ACTUALIZACION*
+  │
+  ├─ ❏ ${prefix}welcome 1
+  └─ La foto del usuario debe estar publica
+
+◪ *RESUELVE TUS DUDAS*
+  │
+  ├─ ❏ ${prefix}creador
+  └─ Dudas o problemas aqui
+
+◪ *CREAR STICKERS*
+  │
+  ├─ ❏ ${prefix}sticker o s
+  ├─ ❏ ${prefix}stickergif
+  ├─ 6 segundos de video
+  ├─ ❏ ${prefix}toimg
+  ├─ De sticker a JPG
+  ├─ ❏ ${prefix}tomp3
+  └─ De video a MP3
+
+◪ *DESCARGAS*
+  │
+  ├─ ❏ ${prefix}ytmp4
+  ├─ Mas link del video de YT
+  ├─ ❏ ${prefix}ytmp3
+  └─ Mas link del video de YT
+
+◪ *AUDIO*
+  │
+  ├─ ❏ ${prefix}play
+  ├─ Mas nombre y artista
+  ├─ ❏ ${prefix}bahasa
+  └─ ❏ ${prefix}tts es
+
+◪ *OTROS*
+  │
+  ├─ ❏ ${prefix}wame
+  ├─ Link de Whatsapp
+  ├─❏ ${prefix}qrcode
+  └─ Coloca un texto
+
+◪ *GRUPOS*
+  │
+  ├─ ❏ ${prefix}closecg
+  ├─ Cerrar el grupo solo admins
+  ├─ ❏ ${prefix}opencg
+  ├─ Abrir grupo solo admins
+  ├─ ❏ ${prefix}kick o pafuera
+  ├─ Eliminar a un miembro 
+  ├─ ❏ ${prefix}promote
+  ├─ Dar admin a un miembro
+  ├─ ❏ ${prefix}demote
+  ├─ Quitar el admin
+  ├─ ❏ ${prefix}linkgc
+  ├─ Link del grupo
+  ├─ ❏ ${prefix}tagall
+  └─ Nombra a todos los del grupo
+
+Para usar estas funciones el bot necesita admin
+  
+◪ *NSWF NUEVAS FUNCIONES* 
+  │
+  └─ ❏ ${prefix}nsfwmenu
+
+Para activar los NSFW coloque el siguiente comando ${prefix}nsfw 1 y para desactivar los NSFW coloque ${prefix}nsfw 0
+
+Grupo del Bot: https://chat.whatsapp.com/Deqdduv784WJpZrCxNdoVg
+`
+}
+
+exports.help = help
